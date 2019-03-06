8b885844d6cdd77bd8ce10dc24ee16072f878109
shrutinavagre <shruti.navagre@gmail.com>
2019-03-06 11:10:54 -0800

    first commit

--- config/database.js
+++ config/database.js
@@ -1,9 +1,5 @@
-if (process.env.NODE_ENV === 'production') {
-  module.exports = {
-    mongoURI: 'mongodb://dipaksingbhukwal:Sadguru@24@ds157853.mlab.com:57853/vidjot-csuf-prod'
-  }
+if(process.env.NODE_ENV === 'production'){
+  module.exports = {mongoURI: 'mongodb://dipaksingbhukwal:Sadguru@24@ds157853.mlab.com:57853/vidjot-csuf-prod'}
 } else {
-  module.expoerts = {
-    mongoURI: 'mongodb://localhost/vidjot-dev'
-  }
+  module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
 }
